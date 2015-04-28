cb2d4e7e6085c3d78d4b87fcc6052b831381105f
LomYen <thanasak_pound@hotmail.com>
2015-04-28 22:31:18 +0700

    Update Program1

--- Program1.c
+++ Program1.c
@@ -2,10 +2,10 @@
 #include <string.h>
 void main()
 {
-	char Name[40], G[5], answer[10];
+	char Name[40], G[5], Answer[10];
 	int i;
 	do
 	{
-		printf("What is your LastName? ");
+		printf("What is ur LastName? ");
 		scanf("%s", Name);
 }
